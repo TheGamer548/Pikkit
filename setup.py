3565e03ad7a3d7329d13860d8ed4ba69f3d826b8
TheGamer548 <106841300+TheGamer548@users.noreply.github.com>
2022-06-17 23:19:22 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="Pikit",   #Name
+    version="0.0.1",    #Version
+    author = "Coder10",  #Me
+    description="Pick tools with'from tools import {pip module}' ", #Description
+    packages=["pikit"] #package
+)
