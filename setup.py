a0eb84497cf2b4249a98fce10779950a8776736d
TheGamer548 <106841300+TheGamer548@users.noreply.github.com>
2022-06-18 09:28:07 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="Pikit",   #Name
-    version="0.0.1",    #Version
-    author = "Coder10",  #Me
-    description="Pick tools with'from tools import {pip module}' ", #Description
-    packages=["pikit"] #package
-)
